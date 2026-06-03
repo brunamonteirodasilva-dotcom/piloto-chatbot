b09319477f83e70bffb4b4f84f0492033c472a08
brunamonteirodasilva-dotcom <brunamonteirodasilva@gmail.com>
2026-06-03 15:44:03 -0400

    Update chatbot.js

--- css/js/chatbot.js
+++ css/js/chatbot.js
@@ -1,3 +1,19 @@
-console.log(document.getElementById("chatButton"));
-console.log(document.getElementById("chatWindow"));
-console.log(document.getElementById("closeChat"));
+window.onload = function() {
+
+    const chatButton = document.getElementById("chatButton");
+    const chatWindow = document.getElementById("chatWindow");
+    const closeChat = document.getElementById("closeChat");
+
+    console.log("Botão:", chatButton);
+    console.log("Janela:", chatWindow);
+
+    chatButton.onclick = function() {
+        console.log("CLICOU");
+        chatWindow.style.display = "block";
+    };
+
+    closeChat.onclick = function() {
+        chatWindow.style.display = "none";
+    };
+
+};
