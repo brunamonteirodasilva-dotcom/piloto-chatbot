41b336ceca8fb332716eb7d8fd302fb89eff8dd1
brunamonteirodasilva-dotcom <brunamonteirodasilva@gmail.com>
2026-08-13 15:37:17 -0400

    Update conhecimento.js

--- css/js/data/conhecimento.js
+++ css/js/data/conhecimento.js
@@ -1,1 +1,187 @@
+const conhecimento = [
 
+  {
+    tema: "credenciamento",
+    perguntas: [
+      "como faço meu cadastro",
+      "como me credencio",
+      "como fazer o credenciamento",
+      "como me credenciar no hugv",
+      "como cadastrar pesquisador",
+      "quero fazer pesquisa no hugv"
+    ],
+    resposta:
+      "Para realizar uma pesquisa no HUGV-UFAM, o primeiro passo é criar uma conta no portal GOV.BR, caso ainda não tenha. Depois, acesse a Rede Pesquisa com seu usuário e senha GOV.BR, preencha o cadastro com seus dados pessoais, link do Currículo Lattes e número do ORCID. Em seguida, localize o HUGV-UFAM na lista de hospitais e clique em 'Solicitar Credenciamento'. A análise da solicitação e a resposta são realizadas por e-mail em até 3 dias úteis. Toda a equipe do projeto deve solicitar credenciamento."
+  },
+
+  {
+    tema: "cadastro do projeto",
+    perguntas: [
+      "como cadastrar um projeto",
+      "como cadastrar minha pesquisa",
+      "onde cadastro o projeto",
+      "como submeter projeto",
+      "como enviar projeto para a gep",
+      "como faço para enviar minha pesquisa"
+    ],
+    resposta:
+      "Após o credenciamento, o pesquisador principal deve acessar a Rede Pesquisa, fazer login, clicar em 'Novo Projeto' e preencher as informações solicitadas. Toda a equipe de pesquisadores deve ser vinculada ao projeto. Ao finalizar e revisar o cadastro, o projeto deve ser enviado para avaliação da GEP."
+  },
+
+  {
+    tema: "quem pode cadastrar",
+    perguntas: [
+      "quem pode cadastrar o projeto",
+      "aluno pode cadastrar projeto",
+      "estudante pode cadastrar pesquisa",
+      "aluno de graduação pode cadastrar",
+      "quem deve cadastrar a pesquisa"
+    ],
+    resposta:
+      "O cadastro do projeto é responsabilidade do pesquisador principal. Aluno de graduação não pode cadastrar o projeto; nesse caso, o cadastro deve ser realizado pelo orientador."
+  },
+
+  {
+    tema: "documentos",
+    perguntas: [
+      "quais documentos preciso",
+      "quais documentos preciso anexar",
+      "documentos para pesquisa",
+      "o que preciso anexar",
+      "quais documentos são necessários"
+    ],
+    resposta:
+      "Para o cadastro do projeto na Rede Pesquisa, devem ser anexados, em formato PDF pesquisável e com boa resolução: Modelo de Projeto de Pesquisa, Resumo Executivo do Projeto, TCLE e/ou TALE quando aplicável, Planilha Orçamentária, Cronograma, Termo de Compromisso de Uso de Dados (TCUD) quando aplicável e Termo de Compromisso do Pesquisador."
+  },
+
+  {
+    tema: "TCUD",
+    perguntas: [
+      "quando preciso do tcud",
+      "o que é tcud",
+      "preciso de tcud",
+      "quando usar termo de compromisso de uso de dados",
+      "pesquisa com dados precisa de tcud"
+    ],
+    resposta:
+      "O Termo de Compromisso de Uso de Dados (TCUD) deve ser utilizado sempre que a pesquisa envolver coleta de dados pessoais ou institucionais."
+  },
+
+  {
+    tema: "avaliação da GEP",
+    perguntas: [
+      "o que acontece depois que envio",
+      "quem avalia minha pesquisa",
+      "a gep avalia o projeto",
+      "quanto tempo demora a avaliação",
+      "qual o prazo da gep",
+      "quanto tempo a gep demora"
+    ],
+    resposta:
+      "Depois que o projeto é enviado, ele é avaliado internamente quanto à viabilidade de execução no HUGV. Um processo SEI é gerado e a documentação referente ao projeto é anexada. Podem ser solicitados ajustes quando necessário. A avaliação da viabilidade ocorre, em média, em 15 dias úteis."
+  },
+
+  {
+    tema: "ajustes",
+    perguntas: [
+      "a gep pode pedir ajustes",
+      "meu projeto voltou para ajuste",
+      "preciso corrigir o projeto",
+      "o que faço se pedirem ajustes",
+      "pediram ajustes na pesquisa"
+    ],
+    resposta:
+      "Sim. Durante a avaliação podem ser solicitados ajustes no projeto ou na documentação. Fique atento ao e-mail cadastrado no sistema, pois é por esse meio que serão enviadas respostas e solicitações de ajustes."
+  },
+
+  {
+    tema: "carta de anuência",
+    perguntas: [
+      "o que é carta de anuência",
+      "quando recebo a carta de anuência",
+      "onde encontro a carta de anuência",
+      "o que faço depois da aprovação da gep",
+      "carta de anuência do hugv"
+    ],
+    resposta:
+      "Após a aprovação do projeto pelo HUGV, a carta de anuência será enviada automaticamente pela Rede Pesquisa e/ou por e-mail aos pesquisadores. Para projetos com seres humanos, a carta deverá ser anexada na Plataforma Brasil para solicitação de apreciação do Comitê de Ética em Pesquisa."
+  },
+
+  {
+    tema: "CEP",
+    perguntas: [
+      "preciso passar pelo cep",
+      "o que faço depois da carta de anuência",
+      "como enviar para o cep",
+      "quando enviar para o comite de etica",
+      "como funciona o cep"
+    ],
+    resposta:
+      "Para projetos com seres humanos, após a aprovação no HUGV e o recebimento da carta de anuência, ela deve ser anexada na Plataforma Brasil para solicitação de apreciação do Comitê de Ética em Pesquisa."
+  },
+
+  {
+    tema: "parecer do CEP",
+    perguntas: [
+      "recebi o parecer do cep",
+      "meu projeto foi aprovado pelo cep",
+      "o que fazer depois da aprovação do cep",
+      "onde colocar o parecer do cep"
+    ],
+    resposta:
+      "Quando receber o parecer do CEP autorizando a pesquisa, acesse novamente a Rede Pesquisa e anexe o Parecer Consubstanciado na aba 'OUTROS DOCUMENTOS'. Depois, solicite pelo e-mail sgpits.hugv-ufam@ebserh.gov.br a emissão do documento 'Encaminhamento de autorização para coleta de dados'."
+  },
+
+  {
+    tema: "autorização para coleta",
+    perguntas: [
+      "posso começar a coleta",
+      "quando posso coletar os dados",
+      "como conseguir autorização para coleta",
+      "autorização para coleta de dados",
+      "já tenho aprovação do cep posso coletar"
+    ],
+    resposta:
+      "Após receber o parecer do CEP autorizando a pesquisa, o pesquisador deve anexá-lo na Rede Pesquisa, na aba 'OUTROS DOCUMENTOS', e solicitar pelo e-mail sgpits.hugv-ufam@ebserh.gov.br a emissão do documento 'Encaminhamento de autorização para coleta de dados' para ter acesso aos dados e/ou às áreas necessárias ao início da pesquisa."
+  },
+
+  {
+    tema: "Plataforma Brasil",
+    perguntas: [
+      "preciso cadastrar na plataforma brasil",
+      "como cadastrar na plataforma brasil",
+      "qual cnpj do hugv",
+      "cnpj do hugv para plataforma brasil",
+      "como vincular o hugv na plataforma brasil"
+    ],
+    resposta:
+      "O projeto deve ser cadastrado na Plataforma Brasil. Quando o HUGV-UFAM for a instituição proponente, deve ser utilizado o CNPJ 15.126.437/0011-15. Caso o pesquisador não tenha vínculo com o HUGV-UFAM, mas deseje vinculá-lo, a instituição deve ser adicionada no campo 'Alterar Meus Dados' da Plataforma Brasil."
+  },
+
+  {
+    tema: "desenvolvimento da pesquisa",
+    perguntas: [
+      "o que faço durante a pesquisa",
+      "como acompanhar minha pesquisa",
+      "preciso atualizar a rede pesquisa",
+      "o que fazer depois que começar a pesquisa",
+      "como acompanhar o projeto"
+    ],
+    resposta:
+      "Durante o desenvolvimento da pesquisa, o pesquisador deve cumprir os compromissos assumidos com o HUGV e registrar periodicamente as informações solicitadas na Rede Pesquisa, mantendo o acompanhamento do estudo atualizado."
+  },
+
+  {
+    tema: "contato",
+    perguntas: [
+      "qual o contato da pesquisa",
+      "qual o email da pesquisa",
+      "como falar com a pesquisa",
+      "telefone da pesquisa",
+      "contato da sgpits"
+    ],
+    resposta:
+      "Para assuntos relacionados à gestão da pesquisa, o contato informado no material institucional é o SGPITS/Unidade de Gestão da Pesquisa. E-mail: sgpits.hugv-ufam@ebserh.gov.br. Telefone: (92) 3305-4797. Atendimento: segunda a sexta-feira, das 8h às 17h."
+  }
+
+];
